40e0febc65c497fbaa538fd39e6c7be3ab9cb7f7
rohansaini01111 <rohansa012@gmail.com>
2026-06-09 09:36:38 +0530

    Update game.js

--- game.js
+++ game.js
@@ -26,6 +26,7 @@ const ship = {
   radius: 10,
   orbitRadius: orbit.outer
 };
+let asteroids = [];
 
 function drawPlanet() {
   ctx.beginPath();
@@ -78,4 +79,42 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
+setInterval(spawnAsteroid, 1000); // har 1 sec me spawn
 animate();
+function spawnAsteroid() {
+  const angle = Math.random() * Math.PI * 2;
+
+  const distance = canvas.width; // bahar se start
+  const x = centerX + distance * Math.cos(angle);
+  const y = centerY + distance * Math.sin(angle);
+
+  asteroids.push({
+    x: x,
+    y: y,
+    radius: 8 + Math.random() * 5,
+    speed: 1 + Math.random() * 1.5,
+    angle: angle
+  });
+}
+
+function drawAsteroids() {
+  asteroids.forEach((a, index) => {
+    // move towards center
+    a.x -= Math.cos(a.angle) * a.speed;
+    a.y -= Math.sin(a.angle) * a.speed;
+
+    ctx.beginPath();
+    ctx.arc(a.x, a.y, a.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#aaa";
+    ctx.fill();
+
+    // remove if center cross
+    const dx = a.x - centerX;
+    const dy = a.y - centerY;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist < 20) {
+      asteroids.splice(index, 1);
+    }
+  });
+}
